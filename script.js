4033498aa4611c75839c8692089cf45232c2f0b3
Mahek shah <149513184+mahekshah429@users.noreply.github.com>
2025-06-21 10:03:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ document.getElementById("change_button").addEventListener("click", function () {
   const blockId = document.getElementById("block_id").value;
   const color = document.getElementById("colour_id").value;
 
-  // First reset all grid blocks to transparent
+  
   const gridItems = document.querySelectorAll(".grid-item");
   gridItems.forEach(item => {
     item.style.backgroundColor = "transparent";
   });
 
-  // Apply color to the selected block
   const selectedBlock = document.getElementById(blockId);
   if (selectedBlock) {
     selectedBlock.style.backgroundColor = color;
@@ -17,14 +16,12 @@ document.getElementById("change_button").addEventListener("click", function () {
   }
 });
 
-// Reset Button Logic
+
 document.getElementById("Reset").addEventListener("click", function () {
   const gridItems = document.querySelectorAll(".grid-item");
   gridItems.forEach(item => {
     item.style.backgroundColor = "transparent";
   });
-
-  // Clear inputs
   document.getElementById("block_id").value = "";
   document.getElementById("colour_id").value = "";
 });
